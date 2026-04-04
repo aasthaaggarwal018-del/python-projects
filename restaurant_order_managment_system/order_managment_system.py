0627bc3122f8fe36a1b52305bd4befbce47c9ab0
aastha <aasthaaggarwal018@gmail.com>
2026-04-04 21:02:01 +0530

    Update order_managment_system.py

--- restaurant_order_managment_system/order_managment_system.py
+++ restaurant_order_managment_system/order_managment_system.py
@@ -1,4 +1,4 @@
-
+# creating a list of orders and dictionary of menu
 order_list=[]
 menu={
             'pizza':300,
@@ -8,26 +8,20 @@ menu={
             'cold drinks':50,
             'milkshakes':70
         }
-
 print ('--welcome to our restaurant. we hope you like our services and food---' )
 
 while True :
-
-
-
+# collecting input to proceeds
     print('\n1. show menu \n2. add order \n3. view orders \n4.total bill amount' \
     '\n5. exit \n')
 
     choice = int(input('entre your choice here :'))
-
+# shows menu
     if choice==1:
-        
         print('\n---menu---')
-        for item,price in menu.items():
-            print(f'{item.capitalize()}:{price}')
-
-
-    
+        for i,j in menu.items-():
+            print(f'{i}:{j}')
+# add order
     elif choice==2:
         item = input('entre your item here:')
         if item in menu:
@@ -44,8 +38,7 @@ while True :
             print('\norder added successfully!')
         else:
             print('\nitem not available')
-
-
+# presenting orders
     elif choice==3:
         if len(order_list)==0:
             print('no order yet')
@@ -56,16 +49,16 @@ while True :
                 print(f'quantity : {i['quantity']}')
                 print(f'price : {i['price']}')
                 count =+1
-
+#calculating total bill
     elif choice==4:
         total_bill=0
         for i in order_list:
             total_bill=total_bill+i['price']
         print(f'\ntotal bill amount is {total_bill}')
-
+# exit 
     elif choice==5:
         print('thanks for visit')
         break
-
+# invalid option
     else:
         print('you entred something wrong')
